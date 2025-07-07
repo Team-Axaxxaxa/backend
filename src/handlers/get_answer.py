6360d59d8a9cb7ace7cb06d19b65e0b7ad4357dc
slash0t <minenion2@gmail.com>
2025-07-08 01:31:50 +0300

    Added get answer endpoint

---
+++ src/handlers/get_answer.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter, Depends, Path, HTTPException
+from pydantic import UUID4
+from sqlalchemy import select, and_
+from sqlalchemy.orm import Session
+from starlette import status
+
+from src.db import get_session
+from src.models import TestTaker, Answer
+from src.schemas.get_answer import GetAnswerResponse
+from src.utils.test_maker_jwt import get_test_taker
+
+api_router = APIRouter(tags=["Url"])
+
+@api_router.get(
+    "/answer/{question_id}",
+    status_code=status.HTTP_200_OK,
+    response_model=GetAnswerResponse,
+    responses={
+        status.HTTP_404_NOT_FOUND: {
+            "description": "Не найден такой ответ",
+        }
+    },
+)
+async def create_answer(
+    question_id: str = Path(...),
+    test_taker: TestTaker = Depends(get_test_taker),
+    session: Session = Depends(get_session),
+):
+    question_uuid = UUID4(question_id)
+
+    answer_query = select(Answer).where(
+        and_(Answer.question == question_uuid, Answer.test_taker == test_taker)
+    )
+    answer = session.scalar(answer_query)
+
+    if not answer:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Не найден такой ответ",
+        )
+
+    return GetAnswerResponse(option=answer.option)
