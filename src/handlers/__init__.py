b59c9c486cd4344510f2e48a10b2fe90e54d1946
slash0t <minenion2@gmail.com>
2025-07-07 22:41:03 +0300

    Added basic healthcheck

---
+++ src/handlers/__init__.py
@@ -0,0 +1,9 @@
+from src.handlers.health_check import api_router as health_router
+
+list_of_routes = [
+    health_router,
+]
+
+__all__ = [
+    "list_of_routes",
+]
---
+++ src/handlers/health_check.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+from starlette import status
+
+api_router = APIRouter(tags=["Health"])
+
+@api_router.get(
+    "/health",
+    status_code=status.HTTP_200_OK,
+)
+def get_health():
+    return {'message': 'healthy!'}
