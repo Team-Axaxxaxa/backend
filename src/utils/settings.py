4a0ed283249ecd611246c514fe54844a714cc772
slash0t <minenion2@gmail.com>
2025-07-07 16:50:52 +0300

    Added settings

---
+++ src/utils/settings.py
@@ -0,0 +1,18 @@
+from os import environ
+
+class Settings:
+    POSTGRES_DB: str = environ.get('POSTGRES_DB', 'db')
+    POSTGRES_USER: str = environ.get('POSTGRES_USER', 'user')
+    POSTGRES_PASSWORD: str = environ.get('POSTGRES_PASSWORD', 'password')
+    POSTGRES_HOST: str = environ.get('POSTGRES_HOST', 'localhost')
+    POSTGRES_PORT: int = int(environ.get('POSTGRES_PORT', 5432))
+
+    @property
+    def db_settings(self) -> dict:
+        return {
+            'database': self.POSTGRES_DB,
+            'user': self.POSTGRES_USER,
+            'password': self.POSTGRES_PASSWORD,
+            'host': self.POSTGRES_HOST,
+            'port': self.POSTGRES_PORT,
+        }
