5e969705f8339629c673a1096f4ab2d372d80e3e
slash0t <minenion2@gmail.com>
2025-07-07 21:58:01 +0300

    Renamed main script

---
+++ src/__main__.py
@@ -0,0 +1,42 @@
+from logging import getLogger
+from urllib.parse import urlparse
+
+from fastapi import FastAPI
+from uvicorn import run
+
+from src.handlers import list_of_routes
+from src.utils.settings import Settings, get_settings
+
+logger = getLogger(__name__)
+
+
+def bind_routes(application: FastAPI, setting: Settings) -> None:
+    for route in list_of_routes:
+        application.include_router(route, prefix=setting.PATH_PREFIX)
+
+
+def get_app() -> FastAPI:
+    application = FastAPI(
+        title='Url shortener',
+        docs_url='/swagger',
+        openapi_url='/openapi',
+        version='1.0.0',
+    )
+    settings = get_settings()
+    bind_routes(application, settings)
+    return application
+
+
+app = get_app()
+
+
+if __name__ == '__main__':  # pragma: no cover
+    settings_for_application = get_settings()
+    run(
+        'src.__main__:app',
+        host=urlparse(settings_for_application.APP_HOST).netloc,
+        port=settings_for_application.APP_PORT,
+        reload=True,
+        reload_dirs=['src', 'tests'],
+        log_level='debug',
+    )
