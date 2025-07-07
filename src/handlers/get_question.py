301eb1b664d14a517b1f049238d022800b902967
slash0t <minenion2@gmail.com>
2025-07-08 01:07:54 +0300

    Added get question handler

---
+++ src/handlers/get_question.py
@@ -0,0 +1,43 @@
+from fastapi import APIRouter, Depends, HTTPException, Path
+from pydantic import UUID4
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+from starlette import status
+
+from src.db import get_session
+from src.models import Question
+from src.schemas.question import QuestionResponse
+
+api_router = APIRouter(tags=["Url"])
+
+@api_router.get(
+    "/question/{id}",
+    status_code=status.HTTP_200_OK,
+    response_model=QuestionResponse,
+    responses= {
+        status.HTTP_404_NOT_FOUND: {
+            "description": "Не найден такой вопрос",
+        }
+    },
+)
+async def get_random_unfilled_question(
+    id: str = Path(...),
+    session: Session = Depends(get_session),
+):
+    uuid = UUID4(id)
+    question_query = select(Question).where(Question.id == uuid)
+    question = session.scalar(question_query)
+
+    if not question:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Не найден такой вопрос",
+        )
+
+    return QuestionResponse(
+        id=question.id,
+        category=question.category,
+        text=question.text,
+        for_male=question.for_male,
+        increase_option=question.increase_option,
+    )
