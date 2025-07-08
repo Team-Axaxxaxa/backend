47b92cb8216c2757a61929259240e189900ee970
slash0t <minenion2@gmail.com>
2025-07-08 15:09:31 +0300

    Added new endpoints to application

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -7,6 +7,8 @@ from src.handlers.get_random_unfilled_question import api_router as get_random_u
 from src.handlers.create_result import api_router as create_result_router
 from src.handlers.get_category_results import api_router as get_category_results_router
 from src.handlers.get_my_result import api_router as get_my_result_router
+from src.handlers.get_answer_count import api_router as get_answer_count_router
+from src.handlers.get_question_count import api_router as get_question_count_router
 
 list_of_routes = [
     health_router,
@@ -18,6 +20,8 @@ list_of_routes = [
     create_result_router,
     get_category_results_router,
     get_my_result_router,
+    get_answer_count_router,
+    get_question_count_router,
 ]
 
 __all__ = [
