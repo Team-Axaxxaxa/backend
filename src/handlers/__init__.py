65992e5608d0128669c3f217830cf711ce51cc36
slash0t <minenion2@gmail.com>
2025-07-08 13:12:21 +0300

    Connected new endpoints to application

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -4,6 +4,9 @@ from src.handlers.create_test_taker import api_router as create_test_taker_route
 from src.handlers.get_answer import api_router as get_answer_router
 from src.handlers.get_question import api_router as get_question_router
 from src.handlers.get_random_unfilled_question import api_router as get_random_unfilled_question_router
+from src.handlers.create_result import api_router as create_result_router
+from src.handlers.get_category_results import api_router as get_category_results_router
+from src.handlers.get_my_result import api_router as get_my_result_router
 
 list_of_routes = [
     health_router,
@@ -12,6 +15,9 @@ list_of_routes = [
     get_answer_router,
     get_question_router,
     get_random_unfilled_question_router,
+    create_result_router,
+    get_category_results_router,
+    get_my_result_router,
 ]
 
 __all__ = [
