c175a4e80f565a82c40768df5ff9e43671c3aed5
slash0t <minenion2@gmail.com>
2025-07-11 21:16:01 +0300

    Updated content generation for ui

--- src/utils/ai_analysis/generate_content.py
+++ src/utils/ai_analysis/generate_content.py
@@ -1,15 +1,15 @@
 from typing import Iterable
 
 from pydantic import UUID4
-from sqlalchemy import select
+from sqlalchemy import select, func
 from sqlalchemy.orm import Session
 
-from src.models import CategoryResult, Category
+from src.models import CategoryResult, Category, QuestionInCategory
 
 
 def generate_content(session: Session, result_id: UUID4) -> str:
     content = ''
-    with open('default_message.txt', 'r') as f:
+    with open('./src/utils/ai_analysis/default_message.txt', 'r') as f:
         content += f.read()
     content += generate_result_text(session, result_id)
     return content
@@ -24,6 +24,12 @@ def generate_result_text(session: Session, result_id: UUID4) -> str:
         category_query = select(Category).where(Category.id == category_result.category)
         category: Category = session.scalar(category_query)
 
-        result_text += f'Для категории {category.name} итоговый результат {category_result.score}\n'
+        max_in_category = (session.query(func.count())
+                        .select_from(QuestionInCategory)
+                        .where(QuestionInCategory.category == category.id)
+                        .scalar())
+
+        result_text += (f'Для категории {category.name} итоговый результат {category_result.score}'
+                        f' при этом максимум {max_in_category}\n')
 
     return result_text
